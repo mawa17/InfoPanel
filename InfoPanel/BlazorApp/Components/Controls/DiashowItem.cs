003d598f0fdb81461a69fb3fbc01deebc955768b
mawa17 <163126157+mawa17@users.noreply.github.com>
2025-05-26 11:04:27 +0200

    Diashow Improvements

---
+++ InfoPanel/BlazorApp/Components/Controls/DiashowItem.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Components.Controls;
+
+public sealed class DiashowItem : ComponentBase
+{
+    [Parameter] public RenderFragment ChildContent { get; set; } = null!;
+    [CascadingParameter] public Diashow? Parent { get; set; }
+    protected override void OnInitialized()
+    {
+        Parent?.RegisterItem(this);
+    }
+}
