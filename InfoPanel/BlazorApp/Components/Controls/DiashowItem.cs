fe7ea80c93e4f0649c7c5f697fffdd86cc583cea
mawa17 <163126157+mawa17@users.noreply.github.com>
2025-06-12 09:56:25 +0200

    Squashed commit of the following:
    commit a652c537a0ba79f2fb002ef0393ebed7effc46a2
    Author: mawa17 <163126157+mawa17@users.noreply.github.com>
    Date:   Thu Jun 12 09:51:32 2025 +0200
    
        Diashow Cleanup

--- InfoPanel/BlazorApp/Components/Controls/DiashowItem.cs
+++ InfoPanel/BlazorApp/Components/Controls/DiashowItem.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Components;
-
 namespace BlazorApp.Components.Controls;
-
 public sealed class DiashowItem : ComponentBase
 {
     [Parameter] public RenderFragment ChildContent { get; set; } = null!;
+    [Parameter] public bool LightMode { get; set; } = false;
     [CascadingParameter] public Diashow? Parent { get; set; }
     protected override void OnInitialized()
     {
