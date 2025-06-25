1df9ee19387f7888ffc05f7f9e3b5310c2dd394b
Bastian <98094980+xLeeVonX@users.noreply.github.com>
2025-06-25 08:51:04 +0200

    Info guide (#3)
    LMGT!

---
+++ InfoPanel/BlazorApp/Components/Controls/InfoPanels/InfoGuide.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+namespace BlazorApp.Components.Controls.InfoPanels;
+
+public partial class InfoGuide
+{
+    [EditorRequired, Parameter]
+    public List<Column> Columns { get; set; }
+}
+
+public sealed record Column
+{
+    public Column() { }
+    public Column(int width, string title, string text)
+    {
+        Width = width;
+        Title = title;
+        Text = text;
+    }
+
+    public int Width { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+}
